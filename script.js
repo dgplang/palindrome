9b6e5035b46b34538ea5deb17af7934855994d93
Plang Gabriel Damwesh <plang.gabriel@gmail.com>
2025-04-10 20:49:15 +0100

    edits some comments

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-// The Algorithm Challenge
-// Return true if the given string is a palindrome. Otherwise, return false.
 // A palindrome is a word or sentence that’s spelled the same way both forward and backward,
 // ignoring punctuation, case, and spacing
-// Note: You’ll need to remove all non-alphanumeric characters (punctuation, spaces and symbols)
+// Note: It is necessary to remove all non-alphanumeric characters (punctuation, spaces and symbols)
 // and turn everything lower case in order to check for palindromes.
 // Strings can be passed with varying formats, such as "racecar", "RaceCar", and "race CAR" among others.
 
@@ -14,10 +12,9 @@ function palindrome(str) {
     cleanedString = str.replace(/[\W_]/gi, '').toLowerCase()
 
     // Step 2. Use the following methods: split, reverse and join on the string
-    // Compare to see if the original string and string after the operations are strictly equal
     reversedString = cleanedString.split('').reverse().join('')
 
-    // Step 3. Check if cleanedString is strictly equals to reversedString and return a Boolean
+    // Step 3. Check if cleanedString is strictly equal to reversedString and return a Boolean
     return cleanedString === reversedString
 
 }
